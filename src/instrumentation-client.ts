c98493bc6456ac0e143fca7c6c74937308d1dd1b
Connie Michelle <cmc@conniemichelleconsulting.com>
2026-04-03 09:35:19 -0700

    fix: disable sendDefaultPii in Sentry for HIPAA compliance

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -23,9 +23,8 @@ Sentry.init({
   // Define how likely Replay events are sampled when an error occurs.
   replaysOnErrorSampleRate: 1.0,
 
-  // Enable sending user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
+  // PII disabled - HIPAA compliance (no IPs, emails, or cookies sent to Sentry)
+  sendDefaultPii: false,
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
